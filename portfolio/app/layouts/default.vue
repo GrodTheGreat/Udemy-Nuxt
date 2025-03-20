9c4a3c6d2db461ac65fd6a695b93ef9cc0471b81
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-20 19:52:34 -0400

    Styling the app with Tailwind CSS

--- portfolio/app/layouts/default.vue
+++ portfolio/app/layouts/default.vue
@@ -1,15 +1,26 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Main</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-    </ul>
-  </nav>
-  <div>
-    <slot></slot>
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+          >G$</NuxtLink
+        >
+      </div>
+
+      <nav class="font-mono">
+        <ul class="flex space-x-4">
+          <li>
+            <NuxtLink to="/">Main</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="p-2">
+      <slot></slot>
+    </main>
   </div>
 </template>
