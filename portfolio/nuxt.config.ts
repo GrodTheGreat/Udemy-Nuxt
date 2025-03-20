5228c1769acf962ae607eaec7c02ba674bb15981
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-20 19:42:44 -0400

    Add files via upload

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: "2025-03-20",
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
+  modules: ["@nuxtjs/tailwindcss"],
 });
