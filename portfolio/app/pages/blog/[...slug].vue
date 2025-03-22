6517e0abf90c0737f8a6d7a82d71ac79796c4f26
Gerard Causse <grod.causse@gmail.com>
2025-03-21 20:18:58 -0400

    Complete Nuxt Content: Static File-Based CMS lesson

---
+++ portfolio/app/pages/blog/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute();
+
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection("content").path(route.path).first();
+});
+</script>
+
+<template>
+  <article>
+    <ContentRenderer v-if="page" :value="page" />
+  </article>
+</template>
--- portfolio/app/pages/blog/[slug].vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-const route = useRoute();
-</script>
-
-<template>
-  <div>We are inside a blog</div>
-  <div>Post slug is {{ route.params.slug }}</div>
-</template>
