c02f9cd87d4a590257fa6f1e6568350ac2903c60
Gerard Causse <grod.causse@gmail.com>
2025-03-22 11:49:27 -0400

    Complete Blog Post List Redesign #1: Building the UI lesson

--- portfolio/app/components/content/BlogPosts.vue
+++ portfolio/app/components/content/BlogPosts.vue
@@ -9,11 +9,30 @@ const { data: pages } = await useAsyncData("blog-list", () => {
 </script>
 
 <template>
-  <section class="not-prose">
+  <section class="not-prose font-mono">
+    <div class="header-column text-gray-400 text-sm">
+      <div>date</div>
+      <div>title</div>
+    </div>
     <ul>
       <li v-for="page in pages" :key="page.title">
-        <NuxtLink :to="page.path">{{ page.title }}</NuxtLink>
+        <NuxtLink
+          :to="page.path"
+          class="column hover:bg-gray-100 dark:hover:bg-gray-800"
+        >
+          <div class="text-gray-500">2025</div>
+          <div>{{ page.title }}</div>
+        </NuxtLink>
       </li>
     </ul>
   </section>
 </template>
+
+<style lang="postcss" scoped>
+.header-column {
+  @apply flex items-center space-x-8 py-2;
+}
+.column {
+  @apply flex items-center space-x-8 py-2 border-t border-gray-200 dark:border-gray-700;
+}
+</style>
