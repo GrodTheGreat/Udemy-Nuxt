ff31964d68263985b171c2056af8184cc504b99b
Gerard Causse <grod.causse@gmail.com>
2025-03-22 11:22:09 -0400

    Complete Querying in Nuxt Content to Display Blog Post List lesson

--- portfolio/app/pages/blog/index.vue
+++ portfolio/app/pages/blog/index.vue
@@ -1,3 +1,19 @@
+<script setup>
+const { data: pages } = await useAsyncData("blog-list", () => {
+  return queryCollection("content")
+    .select("path", "title", "description")
+    .where("path", "LIKE", "%blog%")
+    .all();
+});
+</script>
+
 <template>
   <div>All blog posts!</div>
+  <section>
+    <ul>
+      <li v-for="page in pages" :key="page.title">
+        <NuxtLink :to="page.path">{{ page.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
