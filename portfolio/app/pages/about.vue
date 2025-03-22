b99a6719140dc7b35bd47971a7b7d769b14a10f9
Gerard Causse <grod.causse@gmail.com>
2025-03-21 21:35:06 -0400

    Complete Custom Pages in Markdown & Typography Customization lesson

--- portfolio/app/pages/about.vue
+++ portfolio/app/pages/about.vue
@@ -3,16 +3,24 @@
 //   layout: "another",
 // });
 
-useHead({
-  title: "About",
-});
+// useHead({
+//   title: "About",
+// });
 
-function enableCustomLayout() {
-  setPageLayout("another");
-}
+// function enableCustomLayout() {
+//   setPageLayout("another");
+// }
+const route = useRoute();
+
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection("content").path(route.path).first();
+});
 </script>
 
 <template>
-  <div>This page is about me.</div>
-  <div><button @click="enableCustomLayout">Update layout</button></div>
+  <!-- <div>This page is about me.</div>
+  <div><button @click="enableCustomLayout">Update layout</button></div> -->
+  <article class="prose dark:prose-invert prose-h1:text-2xl">
+    <ContentRenderer v-if="page" :value="page" />
+  </article>
 </template>
