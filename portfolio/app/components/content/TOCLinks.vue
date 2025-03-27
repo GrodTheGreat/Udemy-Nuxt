eb1bb946963dcd0c909e0df7a774a37c11383f15
Gerard Causse <grod.causse@gmail.com>
2025-03-26 20:43:49 -0400

    Complete Table of Contents: Creating a Links Component lesson

---
+++ portfolio/app/components/content/TOCLinks.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  links: Array,
+  level: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const route = useRoute();
+</script>
+
+<template>
+  <ul>
+    <li v-for="link in links" :key="link.id">
+      <NuxtLink
+        :to="{ path: route.path, hash: `#${link.id}` }"
+        :class="{ 'ml-4': level }"
+      >
+        {{ link.text }}
+      </NuxtLink>
+      <TOCLinks :links="link.children" :level="level + 1" />
+    </li>
+  </ul>
+</template>
--- portfolio/app/pages/blog/[...slug].vue
+++ portfolio/app/pages/blog/[...slug].vue
@@ -1,17 +1,34 @@
 <script setup>
+import TOCLinks from "~/components/content/TOCLinks.vue";
+
 const route = useRoute();
 
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection("content").path(route.path).first();
 });
-console.log(page);
 </script>
 
 <template>
   <article
     class="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none"
   >
-    <p>{{ page.body.toc }}</p>
-    <ContentRenderer v-if="page" :value="page" />
+    <div class="grid grid-cols-6 gap-16">
+      <div
+        :class="{
+          'col-span-4': page.body.toc.links.length,
+          'col-span-6': !page.body.toc.links.length,
+        }"
+      >
+        <ContentRenderer v-if="page" :value="page" />
+      </div>
+      <div class="col-span-2 not-prose" v-if="page.body.toc.links.length">
+        <aside class="sticky top-8">
+          <div class="font-semibold mb-2">Table of Contents</div>
+          <nav>
+            <TOCLinks :links="page.body.toc.links" />
+          </nav>
+        </aside>
+      </div>
+    </div>
   </article>
 </template>
